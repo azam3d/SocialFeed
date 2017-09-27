f4bebf1bf39512d1f92692968e523622893eef69
Azam <swooshfx@gmail.com>
2017-09-28 02:22:30 +0800

    Initial Commit

---
+++ SocialFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialFeed
+//
+//  Created by Azam Baderi on 28/09/2017.
+//  Copyright © 2017 Instance Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
